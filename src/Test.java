b084179fa754e310e9ade0862d1d29284f3c9a7f
Administrator <shuangjunYuan1992@163.com>
2017-12-30 16:24:07 +0800

    修改Test

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,8 @@ package com.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("java");
+		System.out.println("javaSE");
+		System.out.println("javaEE");
+
 	}
 }
