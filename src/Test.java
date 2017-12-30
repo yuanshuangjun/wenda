25fdc3947b29204b1eca37b0500706f9c54dd946
“yuanshuangjun <yuanshuangjun@jindankeji.com>
2017-12-30 16:28:09 +0800

    修个test

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("javaSE");
+		System.out.println("javaSE+javaME");
 		System.out.println("javaEE");
 
 	}
