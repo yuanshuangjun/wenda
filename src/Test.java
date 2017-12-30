b40a3148f69bba0c32ea3e5f5024976c4f52b5a6
Administrator <shuangjunYuan1992@163.com>
2017-12-30 16:34:35 +0800

    请问

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,6 @@ package com.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("javaSE+javaME");
-		System.out.println("javaEE");
-
+		System.out.println("java");
 	}
 }
